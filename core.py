4af6ca4e5f4d584b8043073f38f867d2894ac5fb
ziadamraouiii <49720132+ziadamraouiii@users.noreply.github.com>
2026-06-27 02:05:09 +0100

    Update core.py

--- core.py
+++ core.py
@@ -4,22 +4,40 @@ def calculate_poisson(home_exp, away_exp):
     def poisson(k, lam):
         return (lam**k * math.exp(-lam)) / math.factorial(k)
     
+    # مصفوفة احتمالات النتائج (0-0 إلى 5-5 كافية دقيقة)
+    matrix = [[0 for _ in range(6)] for _ in range(6)]
     home_win, draw, away_win = 0, 0, 0
-    # حساب احتمالات النتائج من 0-0 إلى 7-7
-    for i in range(8):
-        for j in range(8):
+    btts_prob = 0
+    over_2_5 = 0
+    
+    for i in range(6):
+        for j in range(6):
             prob = poisson(i, home_exp) * poisson(j, away_exp)
+            matrix[i][j] = prob
+            
+            # النتائج الأساسية
             if i > j: home_win += prob
             elif j > i: away_win += prob
             else: draw += prob
-    return home_win, draw, away_win
+            
+            # الاحتمالات المتقدمة
+            if i > 0 and j > 0: btts_prob += prob
+            if (i + j) > 2.5: over_2_5 += prob
+            
+    return {
+        "home_win": home_win,
+        "draw": draw,
+        "away_win": away_win,
+        "btts": btts_prob,
+        "over_2_5": over_2_5,
+        "over_1_5": 1 - (matrix[0][0] + matrix[1][0] + matrix[0][1]),
+        "double_chance_home": home_win + draw,
+        "double_chance_away": away_win + draw
+    }
 
 def get_moving_average_stats(matches_data):
-    # تطبيق "الوزن الذكي" (أهمية أكبر للمباريات الحديثة)
-    total_weight = 0
-    w_scored = 0
-    w_conceded = 0
     weights = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.35, 0.3, 0.25]
+    w_scored, w_conceded, total_weight = 0, 0, 0
     
     for idx, (sc, cn) in enumerate(matches_data):
         weight = weights[idx] if idx < len(weights) else 0.2
